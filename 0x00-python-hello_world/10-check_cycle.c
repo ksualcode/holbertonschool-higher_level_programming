5e56257355257a08690ad73a5e1de561fccf9e86
Ksualboy <77964799+Ksualboy@users.noreply.github.com>
2021-04-26 16:52:27 -0300

    adding function comment

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+* check_cycle - Checks if the list cycles
+* @list: list to evaluate
+* ------------------------------------------------
+* Return: 1 if cycles, 0 if not
+*/
 int check_cycle(listint_t *list)
 {
 	listint_t *uwu = list;
