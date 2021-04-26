53a0aa5409e2df3a058719dbb375fd85548a7548
Ksualboy <77964799+Ksualboy@users.noreply.github.com>
2021-04-26 16:50:21 -0300

    fixing 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,18 +2,16 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *head, *uwu = list;
+	listint_t *uwu = list;
 
 	if (list == NULL)
 		return(-1);
-
-	head = list;
 
 	for (; uwu;)
 	{
 		list = uwu;
 		uwu = uwu->next;
-		if (list <= uwu && uwu >= head)
+		if (list <= uwu)
 			return(1);
 	}
 	return (0);
