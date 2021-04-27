2f98af8adf6f101351ab530774b23694d5d018fa
Ksualboy <2758@holbertonschool.com>
2021-04-27 17:38:55 +0000

    adding 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10-i-1):
+        if i == 8 and j+i+1 == 9:
+            print("{:d}{:d}".format(i, j+i+1))
+            break
+        print("{:d}{:d}".format(i, j+i+1), end =', ')
