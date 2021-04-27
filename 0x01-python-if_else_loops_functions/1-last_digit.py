561e6d84c7f52fea9aee989b5c1323f0782effb6
Ksualboy <2758@holbertonschool.com>
2021-04-27 15:20:30 +0000

    fixing pep8 and adding incomplete 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	last_digit = (-number % 10) * -1
+    last_digit = (-number % 10) * -1
 else:
-	last_digit = number % 10
+    last_digit = number % 10
 print("last digit of {:d} is {:d} and is ".format(number, last_digit), end='')
 if last_digit > 5:
-	print("greater than 5")
+    print("greater than 5")
 elif last_digit == 0:
-	print("0")
+    print("0")
 else:
-	print("less than 6 and not 0")
+    print("less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in range(100):
+    print(i, end='')
+    if i == 99:
+		
