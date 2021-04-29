709678f209b93e7c109e3f0764c145a991655287
Ksualboy <2758@holbertonschool.com>
2021-04-29 15:45:17 +0000

    fixing 4 and adding 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ import hidden_4
 
 if __name__ == "__main__":
     names = sorted(dir(hidden_4))
-    for n in range(1,len(names)):
+    for n in range(1, len(names)):
         if "__" in names[n]:
             continue
         print(names[n])
