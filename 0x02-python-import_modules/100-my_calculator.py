720c5b17d2d632ac0f520ed64436147c82c60b72
Ksualboy <2758@holbertonschool.com>
2021-04-29 16:37:15 +0000

    adding 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(args[1])
+    b = int(args[3])
+    if args[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif args[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif args[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif args[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
