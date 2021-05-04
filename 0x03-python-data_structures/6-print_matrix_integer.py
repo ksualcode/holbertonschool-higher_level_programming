f6b22d87d7cf8efdff1462231a0b639f205082bd
Ksualboy <2758@holbertonschool.com>
2021-05-04 18:47:37 +0000

    adding 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             print(i[j], end='')
-            if (j != (len(i) - 1)):
+            if j != len(i) - 1:
                 print("", end=' ')
         print()
