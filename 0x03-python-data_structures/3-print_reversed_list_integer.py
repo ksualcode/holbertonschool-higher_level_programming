234447cb822489e9da1b339ba64e9105201c8206
Ksualboy <2758@holbertonschool.com>
2021-05-04 17:42:31 +0000

    adding border case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
+
     for n in range(0, len(my_list)):
         print("{:d}".format(my_list[(-n) - 1]))
