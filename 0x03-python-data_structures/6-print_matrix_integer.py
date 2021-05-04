8e09ebbc0bd02389f165aa08c83d153fa868c3ba
Ksualboy <2758@holbertonschool.com>
2021-05-04 18:36:53 +0000

    pushing 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            print(i[j], end='')
+            if (j != (len(i) - 1)):
+                print("", end=' ')
+        print()
