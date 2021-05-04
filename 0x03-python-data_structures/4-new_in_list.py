93e7e30f49b4381253712ad5b7c2d28e5403e5b7
Ksualboy <2758@holbertonschool.com>
2021-05-04 17:24:44 +0000

    giving up with 3 and adding 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    my_new_list = my_list[:]
+    my_new_list[idx] = element
+    return my_new_list
