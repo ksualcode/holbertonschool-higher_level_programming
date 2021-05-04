4e2f8223d5c097b3928faf080c5ba9e1d9a2c96a
Ksualboy <2758@holbertonschool.com>
2021-05-04 17:16:09 +0000

    trying to satisfy the checker

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for n in range(0, len(my_list)):
-        print("{}".format(my_list[(-n) - 1]))
+        print("{:d}".format(my_list[(-n) - 1]))
