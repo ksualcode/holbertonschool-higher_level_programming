989d5123cd10672cf108d782316d69abe3aaa28c
Ksualboy <2758@holbertonschool.com>
2021-05-06 18:44:57 +0000

    fixing pep8

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i in range(new_list.count(search)):
         new_list[new_list.index(search)] = replace
-    
+
     return(new_list)
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,5 +3,5 @@ def number_keys(a_dictionary):
     n = 0
     for i in a_dictionary:
         n += 1
-    
+
     return n
