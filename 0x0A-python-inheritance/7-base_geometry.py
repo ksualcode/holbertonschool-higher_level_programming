0a5cda48061cde0384e6348cb1432d143b214ad5
Ksualboy <2758@holbertonschool.com>
2021-06-01 23:46:33 +0000

    I'm going mad

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,15 +2,15 @@
 ''' Module with an empty class '''
 
 
-class BaseGeometry:
-    ''' A Base Geometry class '''
+class BaseGeometry():
+    """ Class that's a superclass """
     def area(self):
-        ''' the area of the shape '''
+        ''' raises an exception '''
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        ''' validates an int value '''
-        if (type(value) != int):
+        ''' Validates the value that comes in '''
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        elif (value <= 0):
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
