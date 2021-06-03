4694a13df8275b441b26ba26772ac152fef7f3dc
Ksualboy <2758@holbertonschool.com>
2021-06-03 03:01:25 +0000

    fixing

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,6 @@ import json
 
 
 def load_from_json_file(filename):
-        ''' Function that puts a json in a file '''
+    ''' Function that puts a json in a file '''
     with open(filename, mode="r", encoding="utf-8") as my_file:
         return(json.load(my_file))
