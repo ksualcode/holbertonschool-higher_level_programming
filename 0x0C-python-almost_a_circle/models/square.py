7cd33012c9cd83be7a9585f75d11760b6487223f
Ksualboy <2758@holbertonschool.com>
2021-06-07 23:00:48 +0000

    adding until 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,24 @@ class Square(Rectangle):
         super().value_validator(size, "width")
         self.__width = size
         self.__height = size
+
+    def update(self, *args, **kwargs):
+        ''' Updates a Square '''
+        if args is not None and len(args) != 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 3:
+                self.x = args[2]
+            if len(args) > 4:
+                self.y = args[3]
+
+        elif kwargs is not None:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.__x = kwargs["x"]
+            if "y" in kwargs:
+                self.__y = kwargs["y"]
