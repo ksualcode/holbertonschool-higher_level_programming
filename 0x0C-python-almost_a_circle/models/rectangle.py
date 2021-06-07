b13f0cb8cec61b0c6c10a397d8e3372002cc6705
Ksualboy <2758@holbertonschool.com>
2021-06-07 20:38:11 +0000

    updatig until 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,10 +67,27 @@ class Rectangle(Base):
 
     def display(self):
         ''' Prints the rectangle'''
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
+            print(" " * self.__x, end="")
             print("#" * self.__width)
 
+    def update(self, *args):
+        ''' Updates a Rectangle '''
+        if args is not None and len(args) != 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+
     def __str__(self):
+        ''' prints '''
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
