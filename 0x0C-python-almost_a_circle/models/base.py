e16fa5b6580c543ae64b81edce17ae543fae4db7
Ksualboy <2758@holbertonschool.com>
2021-06-08 16:26:27 +0000

    adding until 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' Module with a base class '''
 
+import json
 
 class Base():
     ''' Superclass for other classes '''
@@ -17,4 +18,16 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        '''  '''
+        ''' Returns a json string representation '''
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' Writes a JSON file '''
+        my_list = {}
+        if list_objs is not None:
+            my_list = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        with open("{}.json".format(cls.__name__), 'w') as file:
+            file.write(my_list)
