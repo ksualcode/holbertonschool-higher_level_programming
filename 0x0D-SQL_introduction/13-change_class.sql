2ccfeec25a53e2136401959c55e1da740a8be1aa
Ksualboy <2758@holbertonschool.com>
2021-07-12 20:15:10 +0000

    adding a bunch until 14

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Drops all recors with score <= 5
+DELETE FROM second_table WHERE `score` <= 5;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- displays the number of rows
-SELECT COUNT(*) FROM first_table WHERE id = 89 
+SELECT COUNT(*) FROM first_table WHERE id = 89;
