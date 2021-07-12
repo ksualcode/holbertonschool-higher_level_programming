86ecf7f661144d3929c84348b788d92e7c5e99c6
Ksualboy <2758@holbertonschool.com>
2021-07-12 19:11:32 +0000

    adding 6, 7 and 8

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Inserts a new row
+INSERT INTO first_table (`id`, `name`) VALUES (89, 'Holberton School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- displays the number of rows
+SELECT COUNT(*) FROM first_table WHERE id = 89 
