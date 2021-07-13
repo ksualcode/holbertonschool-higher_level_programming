f47d9e839d5d019973d5693887dfb37f2cf2f4ba
Ksualboy <2758@holbertonschool.com>
2021-07-13 15:48:51 +0000

    testing 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
--- 
-https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+SELECT s.title, sg.genre_id FROM tv_show_genres sg INNER JOIN tv_shows s ON sg.show_id = s.id ORDER BY s.title, sg.genre_id;
