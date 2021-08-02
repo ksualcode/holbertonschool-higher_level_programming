4a66353d1ec07aea76915ff6479409db42edbd45
Ksualboy <2758@holbertonschool.com>
2021-08-02 16:07:34 +0000

    fixing 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 const number = process.argv[2];
-let x = '';
 
 if (parseInt(number)) {
   for (let i = 0; i < parseInt(number); i++) {
-    x += 'x';
-  }
-  for (let i = 0; i < parseInt(number); i++) {
-    console.log(x);
+    console.log('x'.repeat(number));
   }
 } else {
   console.log('Missing size');
