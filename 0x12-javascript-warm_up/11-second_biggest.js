12bb31c2999ba3e6bf2c2d473d32d7125659971d
Ksualboy <2758@holbertonschool.com>
2021-08-02 18:11:38 +0000

    fixing 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
-let biggest;
-let second;
+function secondBig () {
+  let biggest;
+  let second;
+  const input = process.argv;
 
-const input = process.argv;
-
-if (input.length == 2 || input.length == 3) {
-  console.log('0');
-  return;
-}
-
-for (let i = 0; i < input.length; i++) {
-  if (parseInt(input[2 + i]) > biggest || biggest == undefined) {
-    second = biggest;
-    biggest = parseInt(input[2 + i]);
-  } else if (second == undefined) {
-    second = parseInt(input[2 + i]);
+  if (input.length === 2 || input.length === 3) {
+    return 0;
   }
+
+  for (let i = 0; i < input.length; i++) {
+    if (parseInt(input[2 + i]) > biggest || biggest === undefined) {
+      second = biggest;
+      biggest = parseInt(input[2 + i]);
+    } else if (second === undefined) {
+      second = parseInt(input[2 + i]);
+    }
+  }
+  return second;
 }
-console.log(second);
+
+console.log(secondBig());
