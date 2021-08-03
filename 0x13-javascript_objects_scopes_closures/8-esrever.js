27f53e89408045a72ccf6f83f1dc2e43ff2874d9
Ksualboy <2758@holbertonschool.com>
2021-08-03 17:06:31 +0000

    adding 8 and 9

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const reverse = [];
+  let j = 0;
+  for (let i = list.length - 1; i >= 0; i--, j++) {
+    reverse[j] = list[i];
+  }
+  return (reverse);
+};
