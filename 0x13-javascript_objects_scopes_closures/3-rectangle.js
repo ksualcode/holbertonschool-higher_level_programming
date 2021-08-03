9e158e1b055c9fe52a31f15353035cbc80c7a8de
Ksualboy <2758@holbertonschool.com>
2021-08-03 17:14:21 +0000

    fixing a sintatic thing 2, 3 and 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0 && w && h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print () {
+  print() {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      console.log("X".repeat(this.width));
     }
   }
 };
