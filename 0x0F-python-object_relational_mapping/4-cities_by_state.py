ca27a5659246c4ca7457997c5f2cd12fe4f7e0c3
Ksualboy <2758@holbertonschool.com>
2021-08-08 18:10:03 +0000

    fixing cities order 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                                 user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    sqlquery = "SELECT S.id, C.name, S.name FROM states S"
+    sqlquery = "SELECT C.id, C.name, S.name FROM states S"
     sqlquery += " JOIN cities C ON S.id = C.state_id"
-    sqlquery += " ORDER BY S.id ASC"
+    sqlquery += " ORDER BY C.id ASC"
     cur.execute(sqlquery)
     query_rows = cur.fetchall()
     for row in query_rows:
