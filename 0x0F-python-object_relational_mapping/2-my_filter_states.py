c896d7bb26ca38aca626a0c249ce30f41b96824d
Ksualboy <2758@holbertonschool.com>
2021-08-08 17:02:07 +0000

    fixing like binary 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                                 user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    sqlquery = "SELECT * FROM states WHERE name = '{}'".format(argv[4])
+    sqlquery = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
     sqlquery += "ORDER BY id ASC;"
     cur.execute(sqlquery)
     query_rows = cur.fetchall()
