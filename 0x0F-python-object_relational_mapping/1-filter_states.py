c65becfe6386babfceb1dd19e81a14e7d05a3afe
Ksualboy <2758@holbertonschool.com>
2021-08-08 16:37:57 +0000

    adding readme, 0 to 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+# Learning about Object-relational mapping
