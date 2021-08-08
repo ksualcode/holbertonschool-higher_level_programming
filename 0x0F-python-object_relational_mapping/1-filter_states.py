b82765b7eacdc75b04f941a67f29e28ee3cad423
Ksualboy <2758@holbertonschool.com>
2021-08-08 17:04:31 +0000

    fixing pep8 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                                 user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                ORDER BY id ASC")
+    sqlquery = "SELECT * FROM states WHERE name LIKE BINARY 'N%'"
+    sqlquery += " ORDER BY id ASC"
+    cur.execute(sqlquery)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                                 user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    sqlquery = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
-    sqlquery += "ORDER BY id ASC;"
+    sqlquery = "SELECT * FROM states "
+    sqlquery += "WHERE name LIKE BINARY '{}'".format(argv[4])
+    sqlquery += " ORDER BY id ASC;"
     cur.execute(sqlquery)
     query_rows = cur.fetchall()
     for row in query_rows:
