5b65cd6e5e911ca170fd0d6f4271d237ee375dab
Ksualboy <2758@holbertonschool.com>
2021-08-08 19:23:50 +0000

    adding 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Learning ORM '''
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    ''' State class '''
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
