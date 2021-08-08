f536e7593c8b01e0ba7d9124dfadd48883378f7a
Ksualboy <2758@holbertonschool.com>
2021-08-08 16:42:41 +0000

    fixing if name

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-import MySQLdb
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(argv[4]))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
