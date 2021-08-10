5945236e7e2c4ce3d55ac34b68f733e6db15854c
Ksualboy <2758@holbertonschool.com>
2021-08-10 17:46:45 +0000

    seeing if the checker is broken

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Prints the first state"""
 
+from sys import argv
+from relationship_state import Base, State, City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 if __name__ == "__main__":
-
-	from sys import argv
-	from relationship_state import Base, State, City
-	from sqlalchemy import (create_engine)
-	from sqlalchemy.orm import sessionmaker
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
