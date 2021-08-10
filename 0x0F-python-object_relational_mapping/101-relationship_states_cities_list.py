661b9e4b9740f815fa12e3efeb0d1fd4a3ab00ee
Ksualboy <2758@holbertonschool.com>
2021-08-10 17:54:04 +0000

    aaaaaAAAaaAaAa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -2,8 +2,9 @@
 """Prints the first state"""
 
 from sys import argv
-from relationship_state import Base, State, City
-from sqlalchemy import (create_engine)
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
