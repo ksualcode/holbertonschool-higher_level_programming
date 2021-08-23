1626e4818fd5fba2dc5347ff7aa0774e66b6941b
Daniel Millan <danielmillanalvarez@gmail.com>
2021-08-23 20:55:22 +0000

    fixing main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 ''' script that fetches https://intranet.hbtn.io/status '''
-from urllib import request
 
-with request.urlopen('https://intranet.hbtn.io/status') as response:
-    respuesta = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(respuesta))
-    print("\t- content: {}".format(type(respuesta)))
-    print("\t- utf8 content: {}".format(respuesta.decode("utf-8")))
+if __name__ == "__main__":
+    from urllib import request
+
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        respuesta = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(respuesta))
+        print("\t- content: {}".format(type(respuesta)))
+        print("\t- utf8 content: {}".format(respuesta.decode("utf-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 ''' takes in a URL, sends a request to the URL '''
-from urllib import request, response
-from sys import argv
 
-req = request.Request(argv[1])
-with request.urlopen(req) as response:
-    respuesta = response.info()
-    print(respuesta["X-Request-Id"])
+if __name__ == "__main__":
+    from urllib import request, response
+    from sys import argv
+
+    req = request.Request(argv[1])
+    with request.urlopen(req) as response:
+        respuesta = response.info()
+        print(respuesta["X-Request-Id"])
