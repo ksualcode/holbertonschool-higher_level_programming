009cc4b8470364dfaef8040652e79a856efe3704
Daniel Millan <danielmillanalvarez@gmail.com>
2021-09-14 16:00:23 +0000

    adding 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/'+process.argv[2],
+  method: 'GET',
+};
+
+request(options, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body)["title"]);
+  }
+});
