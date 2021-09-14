aaaa4c4da7df3d09deb082df8fc4bd9f840812e0
Daniel Millan <danielmillanalvarez@gmail.com>
2021-09-14 18:19:02 +0000

    adding 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const counter = {};
+    let lastUser = 1;
+    let count = 0;
+    const tasks = (JSON.parse(body));
+    tasks.forEach(task => {
+      if (task.userId !== lastUser) {
+        console.log(task.userId);
+        counter[lastUser] = count;
+        lastUser = task.userId;
+        count = 0;
+      }
+      if (task.completed) {
+        count++;
+      }
+    });
+    counter[lastUser] = count;
+    console.log(counter);
+  }
+});
