78eff5bf66e9cf6eb78c8b99ffeeaba322a2c1f0
Daniel Millan <danielmillanalvarez@gmail.com>
2021-09-14 16:30:27 +0000

    adding 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, function (error, response, body) {
+    if (error) {
+
+        console.log(error);
+
+    }
+    else {
+
+        const movies = JSON.parse(body).results;
+        let counter = 0;
+        for (let i = 0; i < movies.length; i++) {
+
+            let chars = movies[i]['characters'];
+            for (let j = 0; j < chars.length; j++) {
+                if (chars[j] == 'https://swapi-api.hbtn.io/api/people/18/') {
+                    counter++;
+                    break;
+                }
+                
+            }
+
+        }
+
+        console.log(counter);
+    }
+});
